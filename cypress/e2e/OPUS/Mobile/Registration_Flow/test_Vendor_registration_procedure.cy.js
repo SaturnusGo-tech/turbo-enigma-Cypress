7e0d653247e2f4008780373d13ba22ffed718308
mercuryrucks <mercury@local-internet.ru>
2023-11-09 16:24:41 -0300

    Test: Mobile vendor test has been implemented

---
+++ cypress/e2e/OPUS/Mobile/Registration_Flow/test_Vendor_registration_procedure.cy.js
@@ -0,0 +1,26 @@
+import Supplier_Enrollment_Flow_Verification from '../../../../support/Core_web/HomePage/Supplier_Enrollment_Flow_Verification/Supplier_Registration_Actions/Registration';
+import LoginPage from "../../../../support/Critical_Path/page_objects/LoginPage/LoginPage";
+
+// Test suite for Vendor Registration Procedure
+describe('Vendor registration procedure test', function() {
+
+    // Initializing required page objects for the test
+    const supplier_Enrollment_Flow_Verification = new Supplier_Enrollment_Flow_Verification();
+    const loginPage = new LoginPage();
+
+    // Setup steps to be executed before each test
+    beforeEach(() => {
+        // Navigate to login page
+        cy.viewport(414, 896);
+        loginPage.visit();
+    });
+
+    // Test case to verify successful vendor registration
+    it('should successfully register a new vendor', function() {
+        // Start the registration process
+        supplier_Enrollment_Flow_Verification.CompleteRegistration();
+
+        // Post-registration verification
+        // Additional validation steps will be implement here
+    });
+});
