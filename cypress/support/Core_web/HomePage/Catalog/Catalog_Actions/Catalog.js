ee6f8041a7d97105dd76681ff87c101c0fd2a1ae
mercuryrucks <mercury@local-internet.ru>
2023-11-14 11:21:34 -0300

    Idioma: Re-translate code docs

--- cypress/support/Core_web/HomePage/Catalog/Catalog_Actions/Catalog.js
+++ cypress/support/Core_web/HomePage/Catalog/Catalog_Actions/Catalog.js
@@ -2,26 +2,24 @@ import { CatalogLocators } from '../CatalogLocators/Catalog_Locators';
 
 class CatalogPage {
 
+    // The checkImagePresence function verifies that the image is present, visible, and loaded
+    checkImagePresence(imageLocator, imageAlt) {
+        // Logging: Validating the presence and visibility of the image
+        console.log(`Validating the presence and visibility of image with locator: ${imageLocator}`);
 
- // Функция checkImagePresence проверяет, что изображение присутствует, видимо и загружено
-checkImagePresence(imageLocator, imageAlt) {
-    // Логирование: Валидация присутствия и видимости изображения
-    console.log(`Validating the presence and visibility of image with locator: ${imageLocator}`);
+        // Get the image element considering the custom timeout
+        // to increase waiting time in case of slow image loading
+        cy.get(imageLocator, { timeout: 10000 }) // Set the timeout to 10 seconds
+          .should('be.visible') // Check that the element is visible
+          .and(($img) => {
+              // Verify that the natural width of the image is greater than 0, indicating it has loaded
+              expect($img[0].naturalWidth, 'Image width').to.be.greaterThan(0);
+          })
+          .and('have.attr', 'alt', imageAlt) // Verify the presence of the alt attribute in the image
 
-    // Получаем элемент изображения с учетом пользовательского таймаута
-    // для увеличения времени ожидания в случае медленной загрузки изображения
-    cy.get(imageLocator, { timeout: 10000 }) // Устанавливаем таймаут в 10 секунд
-      .should('be.visible') // Проверяем, что элемент видим
-      .and(($img) => {
-          // Проверяем, что натуральная ширина изображения больше 0, что указывает на его загрузку
-          expect($img[0].naturalWidth, 'Image width').to.be.greaterThan(0);
-      })
-      .and('have.attr', 'alt', imageAlt) // Проверяем наличие атрибута alt у изображения
-
-    // Если необходимо, проверяем, что изображение можно кликнуть
-    console.log(`Checking clickability of image with locator: ${imageLocator}`);
-    // В этом месте могут быть дополнительные проверки на кликабельность, если это требуется для вашего теста
-}
+        // If necessary, check that the image is clickable
+        console.log(`Checking clickability of image with locator: ${imageLocator}`);
+    }
 
     validateCategoryImages(category) {
         console.log(`Starting validation for ${category.name} images.`);
@@ -29,33 +27,33 @@ checkImagePresence(imageLocator, imageAlt) {
         console.log(`Validated ${category.name} image.`);
     }
 
-     validateAllImages() {
-    const categories = [
-        'Business Products & Services',
-        'Facilities',
-        'Information Technology',
-        'Food',
-        'Furniture',
-        'Furnishings',
-        'Education',
-        'Construction',
-        'Parks & Rec',
-        'Disaster Preparedness & Relief',
-        'Public Works',
-        'Public Safety',
-    ];
+    validateAllImages() {
+        const categories = [
+            'Business Products & Services',
+            'Facilities',
+            'Information Technology',
+            'Food',
+            'Furniture',
+            'Furnishings',
+            'Education',
+            'Construction',
+            'Parks & Rec',
+            'Disaster Preparedness & Relief',
+            'Public Works',
+            'Public Safety',
+        ];
 
-    const imageCategories = categories.map(category => ({
-        name: category,
-        locator: CatalogLocators[`Img_${category.replace(/ & /g, '_').replace(/ /g, '_')}`],
-        alt: CatalogLocators[`Alt_${category.replace(/ & /g, '_').replace(/ /g, '_')}`]
-    }));
+        const imageCategories = categories.map(category => ({
+            name: category,
+            locator: CatalogLocators[`Img_${category.replace(/ & /g, '_').replace(/ /g, '_')}`],
+            alt: CatalogLocators[`Alt_${category.replace(/ & /g, '_').replace(/ /g, '_')}`]
+        }));
 
-    console.log('Starting validation of all images.');
-    imageCategories.forEach(category => {
-        this.validateCategoryImages(category);
-    });
-    console.log('Completed validation of all images.');
+        console.log('Starting validation of all images.');
+        imageCategories.forEach(category => {
+            this.validateCategoryImages(category);
+        });
+        console.log('Completed validation of all images.');
     }
 }
 
