59ac1177f8572d2947eba76771c37fff67adc434
mercuryrucks <mercury@local-internet.ru>
2023-12-12 01:27:52 -0300

    Title: Fix Async DOM Update Issue in Click Events
    Description:
    - Implemented a robust solution for handling asynchronous DOM updates during click events within the HeaderValidator class.
    - Updated the `clickAndCheckEachItem` method to re-query each element before performing a click action. This ensures that the elements are still present and visible in the DOM, addressing issues where elements were becoming detached or altered due to async updates.
    - Added a conditional check to return to the previous page only if more items are left in the iteration, ensuring smoother navigation through the list of elements.

--- cypress/configSelector.js
+++ cypress/configSelector.js
@@ -1,21 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Retrieve the configuration for a given environment.
+ * @param {string} env - The environment name (e.g., 'qa', 'uat', 'demo').
+ * @returns {Object} The configuration object for the specified environment.
+ */
 function getConfig(env) {
+    // Define the path to the configuration file
     const configPath = path.join(__dirname, 'environments.json');
+    // Read the configuration file
     const configFile = fs.readFileSync(configPath);
+    // Parse and return the environment-specific configuration
     const environments = JSON.parse(configFile);
 
     return environments[env];
 }
 
-const currentEnvironment = process.env.CYPRESS_ENV || 'qa'; // Используйте переменную окружения CYPRESS_ENV для определения текущего окружения
+// Determine the current environment using the CYPRESS_ENV environment variable, defaulting to 'qa'
+const currentEnvironment = process.env.CYPRESS_ENV || 'qa';
+// Retrieve the configuration for the current environment
 const config = getConfig(currentEnvironment);
 
+// If the configuration for the current environment is not found, log an error and exit
 if (!config) {
     console.error(`Environment not found: ${currentEnvironment}`);
     process.exit(1);
 }
 
-// Экспорт конфигурации для использования в тестах Cypress
+// Export the configuration for use in Cypress tests
 module.exports = config;
